2767a670acd39fa3dc6e465894549c8c24143065
Vincent Raimondi <vraim001@ucr.edu>
2024-03-25 12:53:57 -0700

    Added some project icons

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Yummy from '@/public/medias/yummy-project.png'
 import YummyTest from '@/public/medias/yummy-test.gif'
 import MembershipPortal from '@/public/medias/membership-portal.png'
+import { SiNextdotjs, SiTailwindcss, SiUnity, SiCsharp, SiGooglecloud } from "react-icons/si";
 import { a } from '@react-spring/web'
 
 export default function Projects({ darkMode }: any) {
@@ -12,13 +13,13 @@ export default function Projects({ darkMode }: any) {
         title: 'ACM Membership Portal',
         description: 'Student portal + admin dashboard to officiate membership, track participation, and provide workshop resources',
         image: MembershipPortal,
-        tech: []
+        tech: [SiNextdotjs, SiTailwindcss, SiGooglecloud]
     },
     {
         title: 'Yummy!',
         description: 'A Virtual Reality cooking game. Player interacts with the kitchen and food + earns points for preparing correctly',
         image: Yummy,
-        tech: []
+        tech: [SiUnity, SiCsharp]
     }
 
   ]
@@ -34,10 +35,10 @@ export default function Projects({ darkMode }: any) {
                         <p className="text-[#202020] font-bold text-xl lg:text-2xl 2xl:text-4xl">{title}</p>
                         <p className="text-[#202020] font-medium text-lg lg:text-xl 2xl:text-3xl">{description}</p>
 
-                        <div className='flex mt-10 text-4xl'>
-                            {/* {tech.map((Icon, index) => (
+                        <div className='flex mt-8 text-4xl'>
+                            {tech.map((Icon, index) => (
                                 <Icon className='mr-8'></Icon>
-                            ))} */}
+                            ))}
                         </div>
                     </span>
 
