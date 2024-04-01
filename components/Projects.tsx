ceb9bbe4191a24329bc1e8f8040440cbd0f1473d
vraimondi04 <vincent837869@gmail.com>
2024-03-31 19:54:17 -0700

    added template for hover image

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Yummy from '@/public/medias/yummy-project.png'
-import YummyTest from '@/public/medias/yummy-test.gif'
+import YummyVideo from '@/public/medias/yummy-video.gif'
 import MembershipPortal from '@/public/medias/membership-portal.png'
 import { SiNextdotjs, SiTailwindcss, SiUnity, SiCsharp, SiGooglecloud, SiFirebase } from "react-icons/si";
 import { a } from '@react-spring/web'
@@ -13,23 +13,26 @@ export default function Projects({ darkMode }: any) {
         title: 'ACM Membership Portal',
         description: 'Student portal + admin dashboard to officiate membership, track participation, and provide workshop resources',
         image: MembershipPortal,
+        hoveredImage: MembershipPortal,
         tech: [SiNextdotjs, SiTailwindcss, SiGooglecloud, SiFirebase]
     },
     {
         title: 'Yummy!',
         description: 'A Virtual Reality cooking game. Player interacts with the kitchen and food + earns points for preparing correctly',
         image: Yummy,
+        hoveredImage: YummyVideo,
         tech: [SiUnity, SiCsharp]
     }
-
   ]
+
+  const [isHover, setHover] = useState(false);
   
   return (
     <>
         <div className='m-4 flex flex-col lg:flex-row justify-evenly'>
-            {projects.map(({title, image, description, tech}) => (
+            {projects.map(({title, image, description, tech, hoveredImage}) => (
                 <div className='bg-slate-200 flex flex-col lg:w-2/5 2xl:w-2/6 items-center mb-6 rounded-md' key={title}>
-                    <Image className='flex rounded-md' src={image} width={800} height={800} alt='Project Image'></Image>
+                    <Image className='flex rounded-md' onMouseEnter={() => setHover(false)} onMouseLeave={() => setHover(true)} src={isHover ? image : hoveredImage} width={800} height={800} alt='Project Image'></Image>
         
                     <span className='m-4 font-serif'>
                         <p className="text-[#202020] font-bold text-xl lg:text-2xl 2xl:text-4xl">{title}</p>
