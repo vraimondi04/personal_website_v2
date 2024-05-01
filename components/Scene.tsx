fff622d18f454896a29eb0d0f2f8dded0c3d72ca
vraimondi04 <vincent837869@gmail.com>
2024-04-30 23:50:26 -0700

    attempt to make the portal display the inner mesh

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,7 +1,7 @@
 'use client';
 import * as THREE from 'three';
 import React, { Suspense, useEffect, useState, useRef } from 'react';
-import { PerspectiveCamera, Environment, MeshTransmissionMaterial, Float, useGLTF, useTexture, OrbitControls } from '@react-three/drei';
+import { PerspectiveCamera, Environment, MeshTransmissionMaterial, Float, useGLTF, useTexture, OrbitControls, MeshPortalMaterial, RoundedBox } from '@react-three/drei';
 import { useSpring } from '@react-spring/core';
 import { a } from '@react-spring/three'
 
@@ -35,33 +35,36 @@ export default function Scene({ setBg, handleMode }: any) {
         <PerspectiveCamera makeDefault position={[0, 0, 10]} fov={75} />
         <ambientLight intensity={0.5} />
         <OrbitControls />
-        <Suspense fallback={null}>
-            <Float>
-                <a.mesh
-                    ref={star} {...nodes.Star005}
-                    scale={wobble}
-                    onPointerOver={() => setHovered(true)}
-                    onPointerOut={() => setHovered(false)}
-                    onPointerDown={() => setDown(true)}
-                    onPointerUp={() => {
-                        setDown(false)
-                        handleMode()
-                        setMode(!mode)
-                        setBg({ background: !mode ? '#202020' : '#f0f0f0'})
-                    }}>
-                    <MeshTransmissionMaterial thickness={0.2} roughness={0} transmission={1} ior={1.2} chromaticAberration={0.6} backside={true}/>
-                </a.mesh>
-                <a.mesh ref={star2} {...nodes.Star005} scale={wobble2}>
-                    <MeshTransmissionMaterial thickness={0.2} roughness={0} transmission={1} ior={1.2} chromaticAberration={0.6} backside={true}/>
-                </a.mesh>
-            </Float>
-            <Environment preset="sunset" />
-        </Suspense>
-        <mesh>
-            <sphereGeometry args={[6, 64, 64]} />
-            <meshStandardMaterial map={map} side={THREE.BackSide} />
-        </mesh>
-
+        <RoundedBox args={[6, 10, 0.1]}>
+            <MeshPortalMaterial>
+                <Suspense fallback={null}>
+                    <Float>
+                        <a.mesh
+                            ref={star} {...nodes.Star005}
+                            scale={wobble}
+                            onPointerOver={() => setHovered(true)}
+                            onPointerOut={() => setHovered(false)}
+                            onPointerDown={() => setDown(true)}
+                            onPointerUp={() => {
+                                setDown(false)
+                                handleMode()
+                                setMode(!mode)
+                                setBg({ background: !mode ? '#202020' : '#f0f0f0'})
+                            }}>
+                            <MeshTransmissionMaterial thickness={0.2} roughness={0} transmission={1} ior={1.2} chromaticAberration={0.6} backside={true}/>
+                        </a.mesh>
+                        <a.mesh ref={star2} {...nodes.Star005} scale={wobble2}>
+                            <MeshTransmissionMaterial thickness={0.2} roughness={0} transmission={1} ior={1.2} chromaticAberration={0.6} backside={true}/>
+                        </a.mesh>
+                    </Float>
+                    <Environment preset="sunset" />
+                </Suspense>
+                <mesh>
+                    <sphereGeometry args={[6, 64, 64]} />
+                    <meshStandardMaterial map={map} side={THREE.BackSide} />
+                </mesh>
+            </MeshPortalMaterial>
+        </RoundedBox>
     </>
     )
 
