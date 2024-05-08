73fbebf5232ec59abd52fdb8e62937a8abf28a77
vraimondi04 <vincent837869@gmail.com>
2024-05-07 23:02:19 -0700

    added portal functionality concept by using only one camera

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,9 +1,11 @@
 'use client';
 import * as THREE from 'three';
 import React, { Suspense, useEffect, useState, useRef } from 'react';
-import { PerspectiveCamera, Environment, MeshTransmissionMaterial, Float, useGLTF, useTexture, OrbitControls, MeshPortalMaterial, RoundedBox } from '@react-three/drei';
+import { PerspectiveCamera, Environment, MeshTransmissionMaterial, Float, useGLTF, useTexture, OrbitControls, MeshPortalMaterial, RoundedBox, CameraControls } from '@react-three/drei';
 import { useSpring } from '@react-spring/core';
 import { a } from '@react-spring/three'
+import { easing } from 'maath';
+import { useFrame, useThree } from '@react-three/fiber';
 
 const animatedStar = a(MeshTransmissionMaterial);
 
@@ -14,9 +16,20 @@ export default function Scene({ setBg, handleMode }: any) {
     const [mode, setMode] = useState(false);
     const [down, setDown] = useState(false);
     const [hovered, setHovered] = useState(false);
-    const map = useTexture('medias/Untitled_Artwork.jpg');
+    const [active, setActive] = useState(null);
+    const controlsRef = useRef<any>();
+    const scene = useThree((state) => state.scene);
 
-    // TODO: attempt react fiber room --> try using animations from procreate as world image
+    useEffect(() => {
+        if (active) {
+            const targetPosition = new THREE.Vector3();
+            scene.getObjectByName(active)?.getWorldPosition(targetPosition);
+            controlsRef.current.setLookAt(0,0,5, targetPosition.x, targetPosition.y, targetPosition.z, true)
+        }
+        else {
+            controlsRef.current.setLookAt(0,0,10, 0,0,0, true)
+        }
+    }, [active]);
 
     useEffect(() => {
         document.body.style.cursor = hovered ? 'none' : ''
@@ -24,48 +37,70 @@ export default function Scene({ setBg, handleMode }: any) {
 
     const [{ wobble, wobble2 }] = useSpring(
         {
-            wobble: down ? 6.2 : hovered ? 6.05 : 6,
-            wobble2: down ? 3.7 : hovered ? 3.55 : 3.5,
+            wobble: down ? 1.2 : hovered ? 1.05 : 1,
+            wobble2: down ? .7 : hovered ? .55 : .5,
         },
         [mode, hovered, down]
     )
 
     return (
     <>
-        <PerspectiveCamera makeDefault position={[0, 0, 10]} fov={75} />
+        {/* <PerspectiveCamera makeDefault position={[0, 0, 10]} fov={75} /> */}
         <ambientLight intensity={0.5} />
-        <OrbitControls />
-        <RoundedBox args={[6, 10, 0.1]}>
-            <MeshPortalMaterial>
-                <Suspense fallback={null}>
-                    <Float>
-                        <a.mesh
-                            ref={star} {...nodes.Star005}
-                            scale={wobble}
-                            onPointerOver={() => setHovered(true)}
-                            onPointerOut={() => setHovered(false)}
-                            onPointerDown={() => setDown(true)}
-                            onPointerUp={() => {
-                                setDown(false)
-                                handleMode()
-                                setMode(!mode)
-                                setBg({ background: !mode ? '#202020' : '#f0f0f0'})
-                            }}>
-                            <MeshTransmissionMaterial thickness={0.2} roughness={0} transmission={1} ior={1.2} chromaticAberration={0.6} backside={true}/>
-                        </a.mesh>
-                        <a.mesh ref={star2} {...nodes.Star005} scale={wobble2}>
-                            <MeshTransmissionMaterial thickness={0.2} roughness={0} transmission={1} ior={1.2} chromaticAberration={0.6} backside={true}/>
-                        </a.mesh>
-                    </Float>
-                    <Environment preset="sunset" />
-                </Suspense>
+        <CameraControls ref={controlsRef} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 6}/>
+        <SceneStage
+            texture={'medias/Untitled_Artwork.jpg'}
+            name={'Star'}
+            active={active}
+            setActive={setActive}
+        >
+            <Float>
+                <a.mesh
+                    ref={star} {...nodes.Star005}
+                    scale={wobble}
+                    onPointerOver={() => setHovered(true)}
+                    onPointerOut={() => setHovered(false)}
+                    onPointerDown={() => setDown(true)}
+                    onPointerUp={() => {
+                        setDown(false)
+                        handleMode()
+                        setMode(!mode)
+                        setBg({ background: !mode ? '#202020' : '#f0f0f0'})
+                    }}>
+                    <MeshTransmissionMaterial thickness={0.2} roughness={0} transmission={1} ior={1.2} chromaticAberration={0.6} backside={true}/>
+                </a.mesh>
+                <a.mesh ref={star2} {...nodes.Star005} scale={wobble}>
+                    <MeshTransmissionMaterial thickness={0.2} roughness={0} transmission={1} ior={1.2} chromaticAberration={0.6} backside={true}/>
+                </a.mesh>
+            </Float>
+        </SceneStage>
+    </>
+    )
+
+}
+
+const SceneStage = ({ children, texture, name, active, setActive, ...props}) => {
+    const map = useTexture(texture);
+    // const portalMaterial = useRef({});
+    const portalMaterial = useRef<any>();
+
+    useFrame((_state, delta) => {
+        const worldOpen = active === name;
+        easing.damp(portalMaterial.current, "blend", worldOpen ? 1 : 0, 0.2, delta);
+    })
+    return <group {...props}>
+        <RoundedBox name={name} args={[3, 4, 0.1]} onDoubleClick={() => setActive(active === name ? null : name)}>
+            <MeshPortalMaterial 
+                ref={portalMaterial}
+                side={THREE.DoubleSide}
+            >
+                {children}
+                <Environment preset="sunset" />
                 <mesh>
                     <sphereGeometry args={[6, 64, 64]} />
                     <meshStandardMaterial map={map} side={THREE.BackSide} />
                 </mesh>
             </MeshPortalMaterial>
         </RoundedBox>
-    </>
-    )
-
+    </group>
 }
